822086c63bf44aaab37da521b61528c2f941df6d
callmestech <callmestech@gmail.com>
2019-12-10 01:38:07 +0300

    setImages created

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 
+import sweeper.Box;
+
 public class JavaSweeper extends JFrame {
 
     private JPanel panel;
@@ -13,6 +15,7 @@ public class JavaSweeper extends JFrame {
     }
 
     private JavaSweeper() {
+        setImages();
         initPanel();
         initFrame();
     }
@@ -26,13 +29,27 @@ public class JavaSweeper extends JFrame {
         setVisible(true);
     }
 
-    private void initPanel(){
-        panel = new JPanel();
-        panel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));
-        add (panel);
+    private void setImages() {
+        for (Box box : Box.values()) {
+            box.image = getImage(box.name().toLowerCase());
+        }
     }
 
-    private Image getImage (String name){
+    private void initPanel() {
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                for (Box box : Box.values()) {
+                    g.drawImage((Image)box.image, box.ordinal() * IMAGE_SIZE, 0, this);
+                }
+            }
+        };
+        panel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));
+        add(panel);
+    }
+
+    private Image getImage(String name) {
         String filename = "img/" + name.toLowerCase() + ".png";
         ImageIcon icon = new ImageIcon(getClass().getResource(filename));
         return icon.getImage();
